d9b3247cac0fcf7a59225d74e482a037ccfe518b
Michaelexe <michaelispahani@gmail.com>
2021-08-23 02:00:17 +0430

    modified everything

--- JS/index.js
+++ JS/index.js
@@ -2,9 +2,11 @@ function skillsClick() {
 	document.getElementById('home-content').style.display = "none"
 	document.getElementById('code-content').style.display = "none"
 	document.getElementById('skills-content').style.display = "block"
+	document.getElementById('contact-me-content').style.display = "none"
 	document.getElementById('home').style.borderRight = "4px solid black"
 	document.getElementById('skills').style.borderRight = "4px solid rgb(0,255,0)"
 	document.getElementById('code').style.borderRight = "4px solid black"
+	document.getElementById('contact').style.borderRight = "4px solid black"
 
 }
 
@@ -14,9 +16,11 @@ function homeClick() {
 	document.getElementById('home-content').style.display = "flex"
 	document.getElementById('skills-content').style.display = "none"
 	document.getElementById('code-content').style.display = "none"
+	document.getElementById('contact-me-content').style.display = "none"
 	document.getElementById('home').style.borderRight = "4px solid cyan"
 	document.getElementById('skills').style.borderRight = "4px solid black"	
 	document.getElementById('code').style.borderRight = "4px solid black"
+	document.getElementById('contact').style.borderRight = "4px solid black"
 }
 
 
@@ -25,9 +29,24 @@ function codeClick() {
 	document.getElementById('home-content').style.display = "none"
 	document.getElementById('skills-content').style.display = "none"
 	document.getElementById('code-content').style.display = "block"
+	document.getElementById('contact-me-content').style.display = "none"
 	document.getElementById('home').style.borderRight = "4px solid black"
 	document.getElementById('skills').style.borderRight = "4px solid black"	
 	document.getElementById('code').style.borderRight = "4px solid yellow"
+	document.getElementById('contact').style.borderRight = "4px solid black"
+}
+
+
+
+function contactMeClick() {
+	document.getElementById('home-content').style.display = "none"
+	document.getElementById('skills-content').style.display = "none"
+	document.getElementById('code-content').style.display = "none"
+	document.getElementById('contact-me-content').style.display = "block"
+	document.getElementById('home').style.borderRight = "4px solid black"
+	document.getElementById('skills').style.borderRight = "4px solid black"	
+	document.getElementById('code').style.borderRight = "4px solid black"
+	document.getElementById('contact').style.borderRight = "4px solid rgb(237,9,144)"
 }
 
 
@@ -146,6 +165,7 @@ function skillsMenuClick() {
 	document.getElementById('home-content').style.display = "none"
 	document.getElementById('code-content').style.display = "none"
 	document.getElementById('skills-content').style.display = "block"
+	document.getElementById('contact-me-content').style.display = "none"
 	document.getElementById('menu-open').style.display = "none"
 
 }
@@ -156,6 +176,7 @@ function homeMenuClick() {
 	document.getElementById('home-content').style.display = "flex"
 	document.getElementById('skills-content').style.display = "none"
 	document.getElementById('code-content').style.display = "none"
+	document.getElementById('contact-me-content').style.display = "none"
 	document.getElementById('menu-open').style.display = "none"
 }
 
@@ -165,5 +186,16 @@ function codeMenuClick() {
 	document.getElementById('home-content').style.display = "none"
 	document.getElementById('skills-content').style.display = "none"
 	document.getElementById('code-content').style.display = "block"
+	document.getElementById('contact-me-content').style.display = "none"
+	document.getElementById('menu-open').style.display = "none"
+}
+
+
+
+function contactMenuClick() {
+	document.getElementById('home-content').style.display = "none"
+	document.getElementById('skills-content').style.display = "none"
+	document.getElementById('code-content').style.display = "none"
+	document.getElementById('contact-me-content').style.display = "block"
 	document.getElementById('menu-open').style.display = "none"
 }
